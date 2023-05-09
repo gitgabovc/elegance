182e2b3b3e757ad4770a2a51c20486363785151a
everth <lvc8829283@gmail.com>
2023-05-09 04:37:11 -0400

    subida inicio elegance

---
+++ micssjs/graficas.js
@@ -0,0 +1,17 @@
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+var $A=0;
+var $B=0;
+
+
+    //objeto para agregar el arreglo
+    var $A = document.querySelector('#NR').value; //objeto para agregar el titulo
+    var $B = document.querySelector('#R').value; //objeto para agregar el descrip 
+    
+    
+
+
+
+
+
